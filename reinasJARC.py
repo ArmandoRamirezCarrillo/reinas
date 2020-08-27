0677bd2042d0c3957bc3f4ef910bf0752be8d883
José Armando Ramírez Carrillo <ing.armandoramirez@outlook.com>
2020-08-27 18:17:24 -0500

    Programa ocho reinas version 1

--- reinasJARC.py
+++ reinasJARC.py
@@ -5,19 +5,19 @@ tamañoTablero = 8
 #funcion para llenar el tablero por primera vez
 def llenarTablero(tamañoTablero):
     lista = []
+
     for i in range(tamañoTablero):
         lista.append(0)
     return lista
 
 def analizandolados(tablero,fila,columna):
-    N = len(tablero)
     #izquierda
     for i in range(columna):
         if(tablero[fila][i] == "R"):
             return False
 
     #derecha
-    for i in range(columna,N,1):
+    for i in range(columna,tamañoTablero,1):
         if(tablero[fila][i] == "R"):
             return False
 
@@ -27,48 +27,49 @@ def analizandolados(tablero,fila,columna):
             return False
         
     #diagonal superior derecha
-    for f,c in zip(range(fila,-1,-1), range(columna,N,1)):
+    for f,c in zip(range(fila,-1,-1), range(columna,tamañoTablero,1)):
         if(tablero[f][c] == "R"):
             return False
 
     #diagonal inferior izquierda
-    for f,c in zip(range(fila,N,1), range(columna,-1,-1)):
+    for f,c in zip(range(fila,tamañoTablero,1), range(columna,-1,-1)):
         if(tablero[f][c] == "R"):
             return False
 
     #diagonal inferior derecha
-    for f,c in zip(range(fila,-1,-1), range(columna,N,1)):
+    for f,c in zip(range(fila,-1,-1), range(columna,tamañoTablero,1)):
         if(tablero[f][c] == "R"):
             return False
 
     return True
 
+#Verifica si la columna ya tiene o no una Reina
 def columnaLlena(tablero,columna):
-    N = len(tablero)
-    for i in range(N):
+    for i in range(tamañoTablero):
         if(tablero[i][columna] == "R"):
-            return False
-    return True
+            return True
+    return False
 
-
-def acomodandoReinas(tablero,columna):
-    N = len(tablero)
-    if columna >= N:
-        return False
-
-    for i in range(N):
-        if(columnaLlena(tablero,columna) == True):
-            if(analizandolados(tablero,i,columna) == True):
-                tablero[i][columna] = "R"
-                acomodandoReinas(tablero,columna + 1)
-            else:
-                tablero[i][columna] = 0
-        else:
-            acomodandoReinas(tablero,columna + 1)
+def acomodandoReinas(tablero, columna):
+    if(columna >= tamañoTablero):
+        return True
+    
+    if(columnaLlena(tablero,columna) == True):
+        if(acomodandoReinas(tablero, columna + 1) == True):
+            return True
         
+    for i in range(tamañoTablero):
+        if(analizandolados(tablero,i,columna)):
+            tablero[i][columna] = "R"
+            if(acomodandoReinas(tablero,columna + 1) == True):
+                return True
+            tablero[i][columna] = 0
+        tablero[i][columna] = 0
+    return False
 
-def solucion(tablero): 
-	for i in range(tamañoTablero): 
+#Imprime el tablero de ajedrez, con la solucion
+def tableroAjedrez(tablero): 
+	for i in range(tamañoTablero):
 		for j in range(tamañoTablero): 
 			print (tablero[i][j], end = " ") 
 		print()
@@ -76,16 +77,18 @@ def solucion(tablero):
 #funcion principal donde se va a ejecutar todo
 def principal():
     tablero = []
+
     for i in range(tamañoTablero):
         tablero.append(llenarTablero(tamañoTablero))
 
     #Colocando a la primera reina
-    #nomre   fila columna = "R"
-    #tablero [1]    [0]   = "R"
-    tablero[7][0] = "R"
+    tablero[0][0] = "R"
+
     #Acomodando las demas reinas
-    acomodandoReinas(tablero,1)
-    # print(tablero)
-    solucion(tablero)
+    if(acomodandoReinas(tablero,0) == True):
+        tableroAjedrez(tablero)
+        print("\nPrograma Finalizado")
+        print("\nJose Armando Ramirez Carrillo")
+        return True
 
 principal()
